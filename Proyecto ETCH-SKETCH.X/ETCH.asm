6edebf0cdc67d6986acff611e1c452ea2838e8b6
dar17320 <dar17320@uvg.edu.gt>
2020-10-27 00:53:12 -0600

    Gran avance
    Se me olvido subir las actualizaciones hace varios dias

--- Proyecto ETCH-SKETCH.X/ETCH.asm
+++ Proyecto ETCH-SKETCH.X/ETCH.asm
@@ -12,72 +12,69 @@
  
  
 GPR_VAR	    UDATA
-ADC_value   RES 1
-BANDERA	    RES 1
-BANDERITA   RES 1
-BANDERAS    RES 1
-BANDERITAS  RES 1
+ADC		RES	1
+W_TEMP		RES	1
+STATUS_TEMP	RES	1
+DISPLAY_LOW1	RES	1
+DISPLAY_HIGH1	RES	1
+VAR_DISPLAY1	RES	1
+EJE1		RES	1
+EJE2		RES	1
+BANDERAS	RES	1
 
 
-RES_VECT    CODE    0X0000
-    GOTO    SETUP
-   
-MAIN_PROG CODE                      ; let linker place main program
+RES_VECT    CODE    0x0000		; processor reset vector
+    GOTO    START			; go to beginning of program
 
-SETUP
-    CALL    CONFIG_OSCILATOR		; configurar el oscilador a 4MHz
-    CALL    CONFIG_IO			; TRIS0 como entrada, ANSEL0 como anal√≥gica
-    ;CALL    CONFIG_ADC			; canal 0, fosc/8, adc on, justificado a la izquierda, Vref interno (0-5V)
-   
-LOOP:
-    CALL    CONVERTIDORES
-    CALL    DELAY_500US			; delay 
-    BSF    ADCON0, GO			; EMPIECE LA CONVERSI√?N
-    BTFSC   ADCON0, GO			; revisa que termin√≥ la conversi√≥n
-    GOTO    $-1
-    MOVF    ADRESH, W
-    MOVWF   ADC_value
-    CALL    CONTBIN
-    GOTO    LOOP
-;------------------------------SUBRUTINAS---------------------------------------
- 
-CONVERTIDORES:
-    BCF	    PORTD, RD0
-    BCF	    PORTD, RD1
-    BTFSC   BANDERA, 0
-    GOTO    CONVERTIDOR_1
-CONVERTIDOR_0:
-    BSF	    PORTD, RD1
-    GOTO    CONV_FIN
-CONVERTIDOR_1:
-    BSF	    PORTD, RD0
-    GOTO    CONV_FIN
-CONV_FIN:    
-    CALL    CONV_B0
-    RETURN
-CONV_B0:
-    BTFSS   BANDERA, 0
-    GOTO    COG_0
-COG_1:
-    BCF	    BANDERA, 0
-    RETURN
-COG_0:
-    BSF	    BANDERA, 0
-    RETURN
+ISR_VECT    CODE    0x0004              ; vector de interrupcion
+PUSH:
+    MOVWF   W_TEMP
+    SWAPF   STATUS, W
+    MOVWF   STATUS_TEMP
+ISR:
+    BTFSS   INTCON, T0IF
+    GOTO    POP
+    BCF	    INTCON, T0IF
+    MOVLW   .217
+    MOVWF   TMR0
     
-DELAY_500US:
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
-    RETURN
-;----------------------------CONFIGURACIONES----------------------------------    
+    CALL    DISPLAY
+      
+ENCENDER_LED:
+    BSF	    PORTA, RA7
+    GOTO    POP
+APAGAR_LED:
+    BCF	    PORTA, RA7
+    GOTO    POP
+       
+POP:
+    SWAPF   STATUS_TEMP, W
+    MOVWF   STATUS
+    SWAPF   W_TEMP, F
+    SWAPF   W_TEMP, W
+    RETFIE
+    
+;-------------------------------PRINCIPAL---------------------------------------
+MAIN_PROG CODE                     
+
+START:
+    CALL    CONFIG_OSCILATOR
+    CALL    CONFIG_IO
+    CALL    CONFIG_ADC			; canal 0, fosc/8, adc on, justificado a la izquierda, Vref interno (0-5V)
+;    CALL    CONFIG_TMR0
+;    CALL    CONFIG_INTERRUPT
+
+LOOP:
+    CALL    DELAY
+    BSF	    ADCON0, GO
+    BTFSC   ADCON0, GO
+    GOTO    $-1
+    MOVFW   ADRESH
+    MOVWF   ADC
+    CALL    VALORDISP
+    CALL    DISPLAY
+    GOTO    LOOP
+
 CONFIG_IO:
     BANKSEL TRISA
     CLRF    TRISA
@@ -85,11 +82,9 @@ CONFIG_IO:
     CLRF    TRISB
     CLRF    TRISC
     CLRF    TRISD
-    CLRF    TRISE
     BANKSEL ANSEL
     CLRF    ANSEL
     COMF    ANSEL
-    CLRF    ANSELH
     BANKSEL PORTA
     CLRF    PORTA
     CLRF    PORTB
@@ -97,40 +92,109 @@ CONFIG_IO:
     CLRF    PORTD
     RETURN
 
-CONFIG_ADC0:
+CONFIG_ADC:
     BANKSEL ADCON1
-    MOVLW   B'00000000'			;4Mhz oscilador
+    MOVLW   B'10000000'			;VOLTAJES DE ENTRADA COMO REFERENCIA/DERECHA
     MOVWF   ADCON1
     BANKSEL ADCON0
-    MOVLW   B'00000001'			;RA0 Y RA1 COMO ENTRADA
+    MOVLW   B'01000001'			;AN0-ADC ENCENDIDO-FOSC/8
     MOVWF   ADCON0
-    BSF	    ANSEL,0			;RA0 COMO ANAL”GICO
     RETURN
 
-    
-CONFIG_ADC1:
-    BANKSEL ADCON1
-    MOVLW   B'00000000'			;4Mhz oscilador
-    MOVWF   ADCON1
-    BANKSEL ADCON0
-    MOVLW   B'00000001'			;RA0 Y RA1 COMO ENTRADA
-    MOVWF   ADCON0
-    BSF	    ANSEL,1			;RA1 COMO ANAL”GICO
-    RETURN    
-    
 CONFIG_OSCILATOR:
     BANKSEL OSCCON
-    MOVLW   B'11000001'
+    MOVLW   B'01100001'			; OSCILADOR INTERNO 4MHz
     MOVWF   OSCCON
     RETURN
- 
-    
-CONTBIN:
-    MOVFW   ADC_value
-    CALL    TABLA
-    MOVWF   PORTC
 
-TABLA:					;Tabla para codificar valores binarios a display para display de anodo comun. 
+;CONFIG_TMR0:
+;    BANKSEL OPTION_REG
+;    BCF	    OPTION_REG, T0CS    ;CICLO DE MAQUINA - 
+;    BCF	    OPTION_REG, PSA     ;PRESCALER 8 BITS
+;    BSF	    OPTION_REG, PS2
+;    BSF	    OPTION_REG, PS1
+;    BSF	    OPTION_REG, PS0
+;    BANKSEL TMR0
+;    MOVLW   .12
+;    MOVWF   TMR0
+;    BCF	    INTCON, T0IF
+ ;   RETURN
+     
+;CONFIG_INTERRUPT:
+;    BSF	    INTCON, GIE
+;    BSF	    INTCON, T0IE
+;    BCF	    INTCON, T0IF
+;    RETURN    
+    
+    
+;////////////////////////////////////// FUNCIONES DE LOOP ///////////////////////////////////////////////////////////
+
+DELAY:
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    RETURN
+
+VALORDISP:
+    MOVFW   ADC
+    MOVWF   EJE1
+    SWAPF   ADC,W
+    MOVWF   EJE2
+
+SEPARAR:
+   MOVF    EJE1, W
+   MOVWF   VAR_DISPLAY1		    ;Crea copia de la variable anterior
+   ANDLW   B'00001111'
+   MOVWF   DISPLAY_LOW1		    ;Mueve la copia a la variable del display
+   ;/////////////////////////// REPITE CON SIGUIENTE VARIABLE //////////////////////////////////////////
+   MOVF    EJE2, W
+   MOVWF   VAR_DISPLAY1
+   ANDLW   B'00001111'
+   MOVWF   DISPLAY_HIGH1
+   
+   RETURN
+
+DISPLAY:
+    CLRF    PORTD
+    BTFSC   BANDERAS, 0
+    GOTO    DISPLAY_1
+DISPLAY_0:
+    MOVF    DISPLAY_LOW1, W
+    CALL    TABLA_7SEG
+    MOVWF   PORTC
+    BSF	    PORTD, RD0			;selecciona el display
+    GOTO    DISP_FINA
+DISPLAY_1:
+    MOVF    DISPLAY_HIGH1, W
+    CALL    TABLA_7SEG
+    MOVWF   PORTC
+    BSF	    PORTD, RD1
+    GOTO    DISP_FINA
+DISP_FINA:    
+    CALL    TOGGLE_A0
+    RETURN
+   
+    
+TOGGLE_A0:
+    BTFSS   BANDERAS, 0
+    GOTO    TOG_A0
+TOG_A1:
+    BCF	    BANDERAS, 0
+    RETURN
+TOG_A0:
+    BSF	    BANDERAS, 0
+    RETURN
+
+
+
+TABLA_7SEG:					;Tabla para codificar valores binarios a display para display de anodo comun. 
     ADDWF   PCL
     RETLW   B'11000000'	;0
     RETLW   B'11111001'	;1
@@ -148,14 +212,4 @@ TABLA:					;Tabla para codificar valores binarios a display para display de anod
 
 
 
-
-
-
-
-
-
-
-
-
-
-END
+END    
